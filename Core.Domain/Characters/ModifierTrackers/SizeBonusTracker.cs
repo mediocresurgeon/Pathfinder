a2c3819e0ec5f44fc0394d40d6b10f225700b5e4
Cameron Lind <mediocresurgeon@gmail.com>
2017-09-27 13:26:26 +1300

    Created SizeBonusTracker.  Size bonuses are almost exclusively dynamic (based on the character's size, a mutable property), and didn't fit with the NonStackingTracker's model of "only storing static values".  Now that IModifierTrackers can store dynamic values, SizeBonusTracker is supported.

---
+++ Core.Domain/Characters/ModifierTrackers/SizeBonusTracker.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Domain.Characters.ModifierTrackers
+{
+	/// <summary>
+	/// A size bonus is a boon granted by virtue of the amount of space a character occupies.
+    /// (Note that penalties due to character size are possible as well.)
+	/// </summary>
+	internal sealed class SizeBonusTracker : NonStackingModifierTracker
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Core.Domain.Characters.ModifierTrackers.SizeBonusTracker"/> class.
+        /// </summary>
+        internal SizeBonusTracker()
+        {
+			// This intentionally blank constructor
+			// allows this class to be instantiated.
+		}
+    }
+}
