ffe261fb2b25dea637863da28dca6724da84fd0d
Cameron Lind <mediocresurgeon@gmail.com>
2017-12-24 18:32:02 +1300

    Created Banded Mail of Luck.

---
+++ Core.Domain.UnitTests/Items/Armor/Paizo/CoreRulebook/BandedMailOfLuckTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Core.Domain.Characters;
+using Core.Domain.Characters.ModifierTrackers;
+using Core.Domain.Characters.Movements;
+using Core.Domain.Characters.Skills;
+using Core.Domain.Items.Armor.Paizo.CoreRulebook;
+using Core.Domain.Spells;
+using Moq;
+using NUnit.Framework;
+
+
+namespace Core.Domain.UnitTests.Items.Armor.Paizo.CoreRulebook
+{
+    [TestFixture]
+    [Parallelizable]
+    public class BandedMailOfLuckTests
+    {
+        [Test(Description = "Ensures sensible defaults for a fresh instance of BandedMailOfLuck.")]
+        public void Default()
+        {
+            // Arrange
+            var armor = new BandedMailOfLuck();
+
+            // Assert
+            Assert.IsTrue(armor.IsMasterwork);
+            Assert.AreEqual(5, armor.GetArmorCheckPenalty());
+            Assert.AreEqual(12, armor.GetCasterLevel());
+            Assert.AreEqual(16, armor.GetHardness());
+            Assert.AreEqual(65, armor.GetHitPoints());
+            Assert.AreEqual(18_900, armor.GetMarketPrice());
+            Assert.That(armor.GetName(),
+                        Has.Exactly(1).Matches<INameFragment>(nf => "Banded Mail of Luck" == nf.Text));
+            Assert.That(armor.GetSchools(),
+                        Has.Exactly(1).Matches<School>(s => School.Enchantment == s));
+            Assert.AreEqual(10, armor.GetArmorBonus());
+            Assert.AreEqual(35, armor.GetWeight());
+        }
+
+
+        [Test(Description = "Ensures that passing a null ICharacter argument to BandedMailofLuck.ApplyTo() throws an exception.")]
+        public void ApplyTo_NullICharacter_Throws()
+        {
+            // Arrange
+            var armor = new BandedMailOfLuck();
+
+            // Act
+            TestDelegate applyTo = () => armor.ApplyTo(null);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(applyTo);
+        }
+
+
+        [Test(Description = "Ensures Banded Mail of Luck will apply an AC bonus and armor check penalties to a character.")]
+        public void ApplyTo_ArmorBonusAndArmorCheckPenalty()
+        {
+            // Arrange
+            var affectedMovement = Mock.Of<IModifierTracker>();
+            var mockAffectedMovement = new Mock<IMovement>();
+            mockAffectedMovement.Setup(m => m.BaseSpeed)
+                                .Returns(6);
+            mockAffectedMovement.Setup(m => m.Penalties)
+                                .Returns(affectedMovement);
+
+            var unaffectedMovement = Mock.Of<IModifierTracker>();
+            var mockUnaffectedMovement = new Mock<IMovement>();
+            mockUnaffectedMovement.Setup(m => m.BaseSpeed)
+                                .Returns((byte?)null);
+            mockUnaffectedMovement.Setup(m => m.Penalties)
+                                .Returns(unaffectedMovement);
+
+            var unaffectedPenalty = Mock.Of<IModifierTracker>();
+            var mockUnaffectedSkill = new Mock<ISkill>();
+            mockUnaffectedSkill.Setup(s => s.ArmorCheckPenaltyApplies)
+                               .Returns(false);
+            mockUnaffectedSkill.Setup(s => s.Penalties)
+                               .Returns(unaffectedPenalty);
+
+            var affectedPenalty = Mock.Of<IModifierTracker>();
+            var mockAffectedSkill = new Mock<ISkill>();
+            mockAffectedSkill.Setup(s => s.ArmorCheckPenaltyApplies)
+                               .Returns(true);
+            mockAffectedSkill.Setup(s => s.Penalties)
+                             .Returns(affectedPenalty);
+
+            var armorBonusTracker = Mock.Of<IModifierTracker>();
+            var maxDexPenaltyTracker = Mock.Of<IModifierTracker>();
+
+            var mockCharacter = new Mock<ICharacter>();
+            mockCharacter.Setup(c => c.ArmorClass.ArmorBonuses)
+                         .Returns(armorBonusTracker);
+            mockCharacter.Setup(c => c.ArmorClass.MaxKeyAbilityScore)
+                         .Returns(maxDexPenaltyTracker);
+            mockCharacter.Setup(c => c.MovementModes.GetAll())
+                         .Returns(new IMovement[] { mockUnaffectedMovement.Object, mockAffectedMovement.Object });
+            mockCharacter.Setup(c => c.Skills.GetAllSkills())
+                         .Returns(new ISkill[] { mockUnaffectedSkill.Object, mockAffectedSkill.Object });
+
+            var armor = new BandedMailOfLuck();
+
+            // Act
+            armor.ApplyTo(mockCharacter.Object);
+
+            // Assert
+            Mock.Get(armorBonusTracker)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 10 == calc())),
+                        "Banded Mail of Luck should add a +10 armor bonus to a character's armor class.");
+            Mock.Get(maxDexPenaltyTracker)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 1 == calc())),
+                        "Banded Mail of Luck should inflict a +1 max dex bonus to a character's armor class.");
+            Mock.Get(affectedMovement)
+                .Verify(bt => bt.Add(It.Is<Func<byte>> (calc => 2 == calc())),
+                        "Banded Mail of Luck should reduce a speed from 30ft to 20ft.");
+            Mock.Get(unaffectedMovement)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 0 == calc())),
+                        "Banded Mail of Luck should not penalize a movement mode which does not have a base speed.");
+            Mock.Get(unaffectedPenalty)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 0 == calc())),
+                        "Banded Mail of Luck should not add penalties to skills where armor check penalties do not apply.");
+            Mock.Get(affectedPenalty)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 5 == calc())),
+                        "Banded Mail of Luck should add a -5 penalty to skills where armor check penalties apply.");
+        }
+    }
+}
---
+++ Core.Domain/Items/Armor/Paizo/CoreRulebook/BandedMailOfLuck.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using Core.Domain.Characters;
+using Core.Domain.Characters.Movements;
+using Core.Domain.Characters.Skills;
+using Core.Domain.Items.Materials.Paizo.CoreRulebook;
+using Core.Domain.Spells;
+
+
+namespace Core.Domain.Items.Armor.Paizo.CoreRulebook
+{
+    /// <summary>
+    /// +3 Banded Mail which allows an attack roll made against the wearer to be rerolled (1/week).
+    /// </summary>
+    public sealed class BandedMailOfLuck : Item, IBandedMail
+    {
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="T:Core.Domain.Items.Armor.Paizo.CoreRulebook.BandedMailOfLuck"/> class.
+        /// </summary>
+        public BandedMailOfLuck()
+            : base()
+        {
+            // Intentionally blank
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Banded Mail of Luck is masterwork.
+        /// </summary>
+        public bool IsMasterwork => true;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Banded Mail of Luck bestows a +10 armor bonus.
+        /// </summary>
+        public byte GetArmorBonus() => 10;
+
+
+        /// <summary>
+        /// Banded Mail of Luck has an armor check penalty of 5.
+        /// </summary>
+        public byte GetArmorCheckPenalty() => 5;
+
+
+        /// <summary>
+        /// Banded Mail of Luck weighs 35 pounds.
+        /// </summary>
+        public override double GetWeight() => 35;
+
+
+        /// <summary>
+        /// Banded Mail of Luck has a market price of 18900gp.
+        /// </summary>
+        public override double GetMarketPrice() => 18_900;
+
+
+        /// <summary>
+        /// Banded Mail of Luck has a caster level of 12.
+        /// </summary>
+        public override byte? GetCasterLevel() => 12;
+
+
+        /// <summary>
+        /// Banded Mail of Luck has a hardness of 16.
+        /// </summary>
+        public override byte GetHardness() => Convert.ToByte(Steel.Hardness + 6);
+
+
+        /// <summary>
+        /// Banded Mail of Luck has 65 hit points.
+        /// </summary>
+        public override ushort GetHitPoints() => 65;
+
+
+        /// <summary>
+        /// Banded Mail of Luck has a maximum dexterity bonus of 1.
+        /// </summary>
+        public byte GetMaximumDexterityBonus() => 1;
+
+
+        /// <summary>
+        /// Returns the name of this item.
+        /// </summary>
+        public override INameFragment[] GetName() => new INameFragment[] {
+            new NameFragment("Banded Mail of Luck",
+                             "http://www.d20pfsrd.com/magic-items/magic-armor/specific-magic-armor/banded-mail-of-luck/")
+        };
+
+
+        /// <summary>
+        /// Banded Mail of Luck has an Enchantment aura.
+        /// </summary>
+        public override School[] GetSchools() => new School[] { School.Enchantment };
+
+
+        /// <summary>
+        /// Applies the effects of Banded Mail of Luck to a character.
+        /// </summary>
+        public void ApplyTo(ICharacter character)
+        {
+            if (null == character)
+                throw new ArgumentNullException(nameof(character), "Argument may not be null.");
+            character.ArmorClass?.ArmorBonuses?.Add(this.GetArmorBonus);
+            character.ArmorClass?.MaxKeyAbilityScore?.Add(this.GetMaximumDexterityBonus);
+            foreach (var skill in character.Skills?.GetAllSkills() ?? Enumerable.Empty<ISkill>())
+            {
+                skill.Penalties?.Add(() => skill.ArmorCheckPenaltyApplies ? this.GetArmorCheckPenalty() : (byte)0);
+            }
+            foreach(var movement in character.MovementModes?.GetAll() ?? Enumerable.Empty<IMovement>())
+            {
+                movement.Penalties.Add(() => {
+                    if (!movement.BaseSpeed.HasValue)
+                        return 0;
+                    var finalSpeed = Math.Floor(.75 * movement.BaseSpeed.Value);
+                    return Convert.ToByte(movement.BaseSpeed.Value - finalSpeed);
+                });
+            }
+        }
+        #endregion
+    }
+}
