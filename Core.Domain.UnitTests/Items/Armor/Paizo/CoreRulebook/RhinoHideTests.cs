f5ae37d92901139610b30fa8b3a32de4ae01d588
Cameron Lind <mediocresurgeon@gmail.com>
2017-12-26 16:33:46 +1300

    Created RhinoHide.

---
+++ Core.Domain.UnitTests/Items/Armor/Paizo/CoreRulebook/RhinoHideTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Core.Domain.Characters;
+using Core.Domain.Characters.ModifierTrackers;
+using Core.Domain.Characters.Movements;
+using Core.Domain.Characters.Skills;
+using Core.Domain.Items.Armor.Paizo.CoreRulebook;
+using Core.Domain.Spells;
+using Moq;
+using NUnit.Framework;
+
+
+namespace Core.Domain.UnitTests.Items.Armor.Paizo.CoreRulebook
+{
+    [TestFixture]
+    [Parallelizable]
+    public class RhinoHideTests
+    {
+        [Test(Description = "Ensures sensible defaults for a fresh instance of RhinoHide.")]
+        public void Default()
+        {
+            // Arrange
+            var armor = new RhinoHide();
+
+            // Assert
+            Assert.IsTrue(armor.IsMasterwork);
+            Assert.AreEqual(1, armor.GetArmorCheckPenalty());
+            Assert.AreEqual(9, armor.GetCasterLevel());
+            Assert.AreEqual(6, armor.GetHardness());
+            Assert.AreEqual(40, armor.GetHitPoints());
+            Assert.AreEqual(5165, armor.GetMarketPrice());
+            Assert.That(armor.GetName(),
+                        Has.Exactly(1).Matches<INameFragment>(nf => "Rhino Hide" == nf.Text));
+            Assert.That(armor.GetSchools(),
+                        Has.Exactly(1).Matches<School>(s => School.Transmutation == s));
+            Assert.AreEqual(6, armor.GetArmorBonus());
+            Assert.AreEqual(25, armor.GetWeight());
+        }
+
+
+        [Test(Description = "Ensures that passing a null ICharacter argument to RhinoHide.ApplyTo() throws an exception.")]
+        public void ApplyTo_NullICharacter_Throws()
+        {
+            // Arrange
+            var armor = new RhinoHide();
+
+            // Act
+            TestDelegate applyTo = () => armor.ApplyTo(null);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(applyTo);
+        }
+
+
+        [Test(Description = "Ensures Banded Mail of Luck will apply an AC bonus and armor check penalties to a character.")]
+        public void ApplyTo_ArmorBonusAndArmorCheckPenalty()
+        {
+            // Arrange
+            var affectedMovement = Mock.Of<IModifierTracker>();
+            var mockAffectedMovement = new Mock<IMovement>();
+            mockAffectedMovement.Setup(m => m.BaseSpeed)
+                                .Returns(6);
+            mockAffectedMovement.Setup(m => m.Penalties)
+                                .Returns(affectedMovement);
+
+            var unaffectedMovement = Mock.Of<IModifierTracker>();
+            var mockUnaffectedMovement = new Mock<IMovement>();
+            mockUnaffectedMovement.Setup(m => m.BaseSpeed)
+                                .Returns((byte?)null);
+            mockUnaffectedMovement.Setup(m => m.Penalties)
+                                .Returns(unaffectedMovement);
+
+            var unaffectedPenalty = Mock.Of<IModifierTracker>();
+            var mockUnaffectedSkill = new Mock<ISkill>();
+            mockUnaffectedSkill.Setup(s => s.ArmorCheckPenaltyApplies)
+                               .Returns(false);
+            mockUnaffectedSkill.Setup(s => s.Penalties)
+                               .Returns(unaffectedPenalty);
+
+            var affectedPenalty = Mock.Of<IModifierTracker>();
+            var mockAffectedSkill = new Mock<ISkill>();
+            mockAffectedSkill.Setup(s => s.ArmorCheckPenaltyApplies)
+                               .Returns(true);
+            mockAffectedSkill.Setup(s => s.Penalties)
+                             .Returns(affectedPenalty);
+
+            var armorBonusTracker = Mock.Of<IModifierTracker>();
+            var maxDexPenaltyTracker = Mock.Of<IModifierTracker>();
+
+            var mockCharacter = new Mock<ICharacter>();
+            mockCharacter.Setup(c => c.ArmorClass.ArmorBonuses)
+                         .Returns(armorBonusTracker);
+            mockCharacter.Setup(c => c.ArmorClass.MaxKeyAbilityScore)
+                         .Returns(maxDexPenaltyTracker);
+            mockCharacter.Setup(c => c.MovementModes.GetAll())
+                         .Returns(new IMovement[] { mockUnaffectedMovement.Object, mockAffectedMovement.Object });
+            mockCharacter.Setup(c => c.Skills.GetAllSkills())
+                         .Returns(new ISkill[] { mockUnaffectedSkill.Object, mockAffectedSkill.Object });
+
+            var armor = new RhinoHide();
+
+            // Act
+            armor.ApplyTo(mockCharacter.Object);
+
+            // Assert
+            Mock.Get(armorBonusTracker)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 6 == calc())),
+                        "Rhino Hide should add a +6 armor bonus to a character's armor class.");
+            Mock.Get(maxDexPenaltyTracker)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 4 == calc())),
+                        "Rhino Hide should inflict a +4 max dex bonus to a character's armor class.");
+            Mock.Get(affectedMovement)
+                .Verify(bt => bt.Add(It.Is<Func<byte>> (calc => 2 == calc())),
+                        "Rhino Hide should reduce a speed from 30ft to 20ft.");
+            Mock.Get(unaffectedMovement)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 0 == calc())),
+                        "Rhino Hide should not penalize a movement mode which does not have a base speed.");
+            Mock.Get(unaffectedPenalty)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 0 == calc())),
+                        "Rhino Hide should not add penalties to skills where armor check penalties do not apply.");
+            Mock.Get(affectedPenalty)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 1 == calc())),
+                        "Rhino Hide should add a -1 penalty to skills where armor check penalties apply.");
+        }
+    }
+}
---
+++ Core.Domain/Items/Armor/Paizo/CoreRulebook/RhinoHide.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using Core.Domain.Characters;
+using Core.Domain.Characters.Movements;
+using Core.Domain.Characters.Skills;
+using Core.Domain.Items.Materials.Paizo.CoreRulebook;
+using Core.Domain.Spells;
+
+
+namespace Core.Domain.Items.Armor.Paizo.CoreRulebook
+{
+    /// <summary>
+    /// +2 hide armor which increases the damage dealt when charging (including monted charges).
+    /// Has a reduced armor check penalty.
+    /// </summary>
+    public sealed class RhinoHide : Item, IHideArmor, IMediumArmor
+    {
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Core.Domain.Items.Armor.Paizo.CoreRulebook.RhinoHide"/> class.
+        /// </summary>
+        public RhinoHide()
+            : base()
+        {
+            // Intentionally blank
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Rhino Hide is masterwork.
+        /// </summary>
+        public bool IsMasterwork => true;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Rhino Hide has a +6 armor bonus.
+        /// </summary>
+        public byte GetArmorBonus() => 6;
+
+
+        /// <summary>
+        /// Rhino Hide has an armor check penalty of -1.
+        /// </summary>
+        public byte GetArmorCheckPenalty() => 1;
+
+
+        /// <summary>
+        /// Rhino Hide has a caster level of 9.
+        /// </summary>
+        public override byte? GetCasterLevel() => 9;
+
+
+        /// <summary>
+        /// Rhino Hide has hardness 6.
+        /// </summary>
+        public override byte GetHardness() => Convert.ToByte(Leather.Hardness + 4);
+
+
+        /// <summary>
+        /// Rhino Hide has 40 hit points.
+        /// </summary>
+        public override ushort GetHitPoints() => 40;
+
+
+        /// <summary>
+        /// Rhino Hide has a market price of 5,165gp.
+        /// </summary>
+        public override double GetMarketPrice() => 5_165;
+
+
+        /// <summary>
+        /// Rhino Hide has a maximum dexterity bonus of +4.
+        /// </summary>
+        public byte GetMaximumDexterityBonus() => 4;
+
+
+        /// <summary>
+        /// Returns the name of this item.
+        /// </summary>
+        public override INameFragment[] GetName() => new INameFragment[] {
+            new NameFragment("Rhino Hide", "http://www.d20pfsrd.com/magic-items/magic-armor/specific-magic-armor/rhino-hide/")
+        };
+
+
+        /// <summary>
+        /// Rhino Hide has a transmutation aura.
+        /// </summary>
+        public override School[] GetSchools() => new School[] { School.Transmutation };
+
+
+        /// <summary>
+        /// Rhino Hide weighs 25 pounds.
+        /// </summary>
+        public override double GetWeight() => 25;
+
+
+        /// <summary>
+        /// Applies the effects of this armor to the character wearing it.
+        /// </summary>
+        /// <param name="character">The character who is wearing this armor.</param>
+        /// <exception cref="System.ArgumentNullException">Thrown when an argument is null.</exception>
+        public void ApplyTo(ICharacter character)
+        {
+            if (null == character)
+                throw new ArgumentNullException(nameof(character), "Argument may not be null.");
+            character.ArmorClass?.ArmorBonuses?.Add(this.GetArmorBonus);
+            character.ArmorClass?.MaxKeyAbilityScore?.Add(this.GetMaximumDexterityBonus);
+            foreach (var skill in character.Skills?.GetAllSkills() ?? Enumerable.Empty<ISkill>()) {
+                skill.Penalties?.Add(() => skill.ArmorCheckPenaltyApplies ? this.GetArmorCheckPenalty() : (byte)0);
+            }
+            foreach (var movement in character.MovementModes?.GetAll() ?? Enumerable.Empty<IMovement>()) {
+                movement.Penalties.Add(() => {
+                    if (!movement.BaseSpeed.HasValue)
+                        return 0;
+                    var finalSpeed = Math.Floor(.75 * movement.BaseSpeed.Value);
+                    return Convert.ToByte(movement.BaseSpeed.Value - finalSpeed);
+                });
+            }
+        }
+        #endregion
+    }
+}
