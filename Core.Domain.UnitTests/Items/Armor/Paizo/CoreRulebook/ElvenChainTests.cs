20eac9affd6d7701614889629dd1e67825adea7c
Cameron Lind <mediocresurgeon@gmail.com>
2017-12-26 13:10:05 +1300

    Created ElvenChain.

---
+++ Core.Domain.UnitTests/Items/Armor/Paizo/CoreRulebook/ElvenChainTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Core.Domain.Characters;
+using Core.Domain.Characters.ModifierTrackers;
+using Core.Domain.Characters.Movements;
+using Core.Domain.Characters.Skills;
+using Core.Domain.Items.Armor.Paizo.CoreRulebook;
+using Core.Domain.Spells;
+using Moq;
+using NUnit.Framework;
+
+
+namespace Core.Domain.UnitTests.Items.Armor.Paizo.CoreRulebook
+{
+    [TestFixture]
+    [Parallelizable]
+    public class ElvenChainTests
+    {
+        [Test(Description = "Ensures sensible defaults for a fresh instance of ElvenChain.")]
+        public void Default()
+        {
+            // Arrange
+            var armor = new ElvenChain();
+
+            // Assert
+            Assert.IsTrue(armor.IsMasterwork);
+            Assert.AreEqual(2, armor.GetArmorCheckPenalty());
+            Assert.IsFalse(armor.GetCasterLevel().HasValue);
+            Assert.AreEqual(15, armor.GetHardness());
+            Assert.AreEqual(30, armor.GetHitPoints());
+            Assert.AreEqual(5150, armor.GetMarketPrice());
+            Assert.That(armor.GetName(),
+                        Has.Exactly(1).Matches<INameFragment>(nf => "Elven Chain" == nf.Text));
+            Assert.IsEmpty(armor.GetSchools());
+            Assert.AreEqual(6, armor.GetArmorBonus());
+            Assert.AreEqual(20, armor.GetWeight());
+        }
+
+
+        [Test(Description = "Ensures that passing a null ICharacter argument to ElvenChain.ApplyTo() throws an exception.")]
+        public void ApplyTo_NullICharacter_Throws()
+        {
+            // Arrange
+            var armor = new ElvenChain();
+
+            // Act
+            TestDelegate applyTo = () => armor.ApplyTo(null);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(applyTo);
+        }
+
+
+        [Test(Description = "Ensures Elven Chain will apply an AC bonus and armor check penalties to a character.")]
+        public void ApplyTo_ArmorBonusAndArmorCheckPenalty()
+        {
+            // Arrange
+            var affectedMovement = Mock.Of<IModifierTracker>();
+            var mockAffectedMovement = new Mock<IMovement>();
+            mockAffectedMovement.Setup(m => m.BaseSpeed)
+                                .Returns(6);
+            mockAffectedMovement.Setup(m => m.Penalties)
+                                .Returns(affectedMovement);
+
+            var unaffectedMovement = Mock.Of<IModifierTracker>();
+            var mockUnaffectedMovement = new Mock<IMovement>();
+            mockUnaffectedMovement.Setup(m => m.BaseSpeed)
+                                .Returns((byte?)null);
+            mockUnaffectedMovement.Setup(m => m.Penalties)
+                                .Returns(unaffectedMovement);
+
+            var unaffectedPenalty = Mock.Of<IModifierTracker>();
+            var mockUnaffectedSkill = new Mock<ISkill>();
+            mockUnaffectedSkill.Setup(s => s.ArmorCheckPenaltyApplies)
+                               .Returns(false);
+            mockUnaffectedSkill.Setup(s => s.Penalties)
+                               .Returns(unaffectedPenalty);
+
+            var affectedPenalty = Mock.Of<IModifierTracker>();
+            var mockAffectedSkill = new Mock<ISkill>();
+            mockAffectedSkill.Setup(s => s.ArmorCheckPenaltyApplies)
+                               .Returns(true);
+            mockAffectedSkill.Setup(s => s.Penalties)
+                             .Returns(affectedPenalty);
+
+            var armorBonusTracker = Mock.Of<IModifierTracker>();
+            var maxDexPenaltyTracker = Mock.Of<IModifierTracker>();
+
+            var mockCharacter = new Mock<ICharacter>();
+            mockCharacter.Setup(c => c.ArmorClass.ArmorBonuses)
+                         .Returns(armorBonusTracker);
+            mockCharacter.Setup(c => c.ArmorClass.MaxKeyAbilityScore)
+                         .Returns(maxDexPenaltyTracker);
+            mockCharacter.Setup(c => c.MovementModes.GetAll())
+                         .Returns(new IMovement[] { mockUnaffectedMovement.Object, mockAffectedMovement.Object });
+            mockCharacter.Setup(c => c.Skills.GetAllSkills())
+                         .Returns(new ISkill[] { mockUnaffectedSkill.Object, mockAffectedSkill.Object });
+
+            var armor = new ElvenChain();
+
+            // Act
+            armor.ApplyTo(mockCharacter.Object);
+
+            // Assert
+            Mock.Get(armorBonusTracker)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 6 == calc())),
+                        "Elven Chain should add a +6 armor bonus to a character's armor class.");
+            Mock.Get(maxDexPenaltyTracker)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 4 == calc())),
+                        "Elven Chain should inflict a +1 max dex bonus to a character's armor class.");
+            Mock.Get(unaffectedPenalty)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 0 == calc())),
+                        "Elven Chain should not add penalties to skills where armor check penalties do not apply.");
+            Mock.Get(affectedPenalty)
+                .Verify(bt => bt.Add(It.Is<Func<byte>>(calc => 2 == calc())),
+                        "Elven Chain should add a -2 penalty to skills where armor check penalties apply.");
+        }
+    }
+}
---
+++ Core.Domain/Items/Armor/Paizo/CoreRulebook/ElvenChain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using Core.Domain.Characters;
+using Core.Domain.Characters.Skills;
+using Core.Domain.Items.Materials.Paizo.CoreRulebook;
+using Core.Domain.Spells;
+
+
+namespace Core.Domain.Items.Armor.Paizo.CoreRulebook
+{
+    /// <summary>
+    /// Non-magical mithral chainmail which counts as light armor.
+    /// </summary>
+    public sealed class ElvenChain : Item, IChainmail, ILightArmor
+    {
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Core.Domain.Items.Armor.Paizo.CoreRulebook.ElvenChain"/> class.
+        /// </summary>
+        public ElvenChain()
+            : base()
+        {
+            // Intentionally blank
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Elven Chain is masterwork.
+        /// </summary>
+        public bool IsMasterwork => true;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Elven Chain has a +6 armor bonus,
+        /// </summary>
+        public byte GetArmorBonus() => 6;
+
+
+        /// <summary>
+        /// Elven Chain has an armor check penalty of -2.
+        /// </summary>
+        public byte GetArmorCheckPenalty() => 2;
+
+
+        /// <summary>
+        /// Elven Chain does not have a caster level.
+        /// </summary>
+        public override byte? GetCasterLevel() => null;
+
+
+        /// <summary>
+        /// Elven Chain has hardness 15.
+        /// </summary>
+        public override byte GetHardness() => Mithral.Hardness;
+
+
+        /// <summary>
+        /// Elven Chain has 30 hit points.
+        /// </summary>
+        public override ushort GetHitPoints() => 30;
+
+
+        /// <summary>
+        /// Elven Chain has a market price of 5150gp.
+        /// </summary>
+        public override double GetMarketPrice() => 5150;
+
+
+        /// <summary>
+        /// Elven Chain has a maximum dexterity bonus of +4.
+        /// </summary>
+        public byte GetMaximumDexterityBonus() => 4;
+
+
+        /// <summary>
+        /// Returns the name of this item.
+        /// </summary>
+        /// <returns>The name.</returns>
+        public override INameFragment[] GetName() => new INameFragment[] {
+            new NameFragment("Elven Chain", "http://www.d20pfsrd.com/magic-items/magic-armor/specific-magic-armor/elven-chain/")
+        };
+
+
+        /// <summary>
+        /// Elven Chain does not have an aura.
+        /// </summary>
+        public override School[] GetSchools() => new School[0];
+
+
+        /// <summary>
+        /// Elven Chain weighs 20 pounds.
+        /// </summary>
+        /// <returns>The weight.</returns>
+        public override double GetWeight() => 20;
+
+
+        /// <summary>
+        /// Applies the effects of this armor to the character who is wearing it.
+        /// </summary>
+        /// <param name="character">The character who is wearing this armor.</param>
+        /// <exception cref="System.ArgumentNullException">Thrown when an argument is null.</exception>
+        public void ApplyTo(ICharacter character)
+        {
+            if (null == character)
+                throw new ArgumentNullException(nameof(character), "Argument may not be null.");
+            character?.ArmorClass?.ArmorBonuses?.Add(this.GetArmorBonus);
+            character?.ArmorClass?.MaxKeyAbilityScore?.Add(this.GetMaximumDexterityBonus);
+            foreach (var skill in character?.Skills?.GetAllSkills() ?? Enumerable.Empty<ISkill>()) {
+                skill.Penalties?.Add(() => skill.ArmorCheckPenaltyApplies ? this.GetArmorCheckPenalty() : (byte)0);
+            }
+        }
+        #endregion
+    }
+}
